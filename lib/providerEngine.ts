18b003beaa7ba131820ce6c8155ac7d5de9d4c95
tahsin112233-png <akash.afnan112233@gmail.com>
2026-04-10 12:27:28 +0600

    Update providerEngine.ts

--- lib/providerEngine.ts
+++ lib/providerEngine.ts
@@ -1,9 +1,14 @@
 import * as moviesdrive from "./providers/moviesdrive/posts";
+import * as myflixbd from "./providers/myflixbd/posts";
 
 export const getProvider = (name: string) => {
   switch (name) {
     case "moviesdrive":
       return moviesdrive;
+
+    case "myflixbd":
+      return myflixbd;
+
     default:
       throw new Error("Provider not found");
   }
